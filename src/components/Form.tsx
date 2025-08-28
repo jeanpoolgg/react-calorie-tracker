3226fdc53547c154204c0c5aed5ed97885b2d9ee
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-08-28 03:35:35 -0500

    style(code): format entire project

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -16,31 +16,31 @@ export const Form = () => {
         })
     }
 
-    
-  return (
-    <form className="space-y-5 bg-white shadow p-10 rounded-lg" >
-        <div className="grid grid-cols-1 gap-3" >
-            <label htmlFor="category" className="font-bold">Categoría:</label>
-            <select value={activity.category} onChange={handleChange}  className="border border-slate-300 p-2 rounded-lg w-full" id="category">
-                {
-                    categories.map((category: Category) => (
-                        <option key={category.id} value={category.id} >{category.name}</option>
-                    ))
-                }
-            </select>
-        </div>
 
-        <div className="grid grid-cols-1 gap-3">
-            <label htmlFor="name" className="font-bold">Actividad:</label>
-            <input value={activity.name} onChange={handleChange}   type="text" id="name" className="border border-slate-300 p-2 rounded-lg" placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta" />
-        </div>
+    return (
+        <form className="space-y-5 bg-white shadow p-10 rounded-lg" >
+            <div className="grid grid-cols-1 gap-3" >
+                <label htmlFor="category" className="font-bold">Categoría:</label>
+                <select value={activity.category} onChange={handleChange} className="border border-slate-300 p-2 rounded-lg w-full" id="category">
+                    {
+                        categories.map((category: Category) => (
+                            <option key={category.id} value={category.id} >{category.name}</option>
+                        ))
+                    }
+                </select>
+            </div>
 
-        <div className="grid grid-cols-1 gap-3">
-            <label htmlFor="calories" className="font-bold">Calorías:</label>
-            <input value={activity.calories} onChange={handleChange}  type="number" id="calories" className="border border-slate-300 p-2 rounded-lg" placeholder="Calorías. ej. 300 o 500" />
-        </div>
+            <div className="grid grid-cols-1 gap-3">
+                <label htmlFor="name" className="font-bold">Actividad:</label>
+                <input value={activity.name} onChange={handleChange} type="text" id="name" className="border border-slate-300 p-2 rounded-lg" placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta" />
+            </div>
 
-        <input type="submit" className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer" value='Guardar comida o Guardar ejercicio' />
-    </form>
-  )
+            <div className="grid grid-cols-1 gap-3">
+                <label htmlFor="calories" className="font-bold">Calorías:</label>
+                <input value={activity.calories} onChange={handleChange} type="number" id="calories" className="border border-slate-300 p-2 rounded-lg" placeholder="Calorías. ej. 300 o 500" />
+            </div>
+
+            <input type="submit" className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer" value='Guardar comida o Guardar ejercicio' />
+        </form>
+    )
 }
